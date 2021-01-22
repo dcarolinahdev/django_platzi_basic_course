368238f8283466b94cf66f850f0ef0721aa6cd14
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2021-01-22 12:12:42 -0500

    Now is using system template

--- posts/views.py
+++ posts/views.py
@@ -1,40 +1,40 @@
-"""Posts views."""
-
 # Django
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Utilities
 from datetime import datetime
+import json
 
 posts = [
 	{
-		'name': 'Mont Blac',
-		'user': 'Yésica Cortés',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://picsum.photos/200/200/?image=1036',
+		'title': 'Sebastian de Belalcazar',
+		'user': {
+			'name': 'Carolina Hernandez',
+			'picture': 'https://qph.fs.quoracdn.net/main-qimg-b80fb8d13f575f717fefb14dd759ded4'
+		},
+		'timestamp': datetime.now().strftime("%b %d, %Y - %H:%M horas"),
+		'photo': 'https://90minutos.co/wp-content/uploads/2020/09/estatua-de-sebastian-de-belalcazar-6819a614-7621-45fa-90ed-16c1afc8529c-e1600469695128.jpg',
 	},
 	{
-		'name': 'Via Láctea',
-		'user': 'C. Vander',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://picsum.photos/200/200/?image=903',
+		'title': 'Ermita',
+		'user': {
+			'name': 'Gabriel Ortiz',
+			'picture': 'https://pbs.twimg.com/profile_images/3091183641/339fb831e1199dac9934661550c726ea.jpeg'
+		},
+		'timestamp': datetime.now().strftime("%b %d, %Y - %H:%M horas"),
+		'photo': 'https://www.elpais.com.co/files/article_main_small/uploads/2019/06/03/5cf5ddcd98ee9.jpeg',
 	},
 	{
-		'name': 'Nuevo auditorio',
-		'user': 'Thespianartist',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://picsum.photos/200/200/?image=1076',
+		'title': 'Tres Cruces',
+		'user': {
+			'name': 'David Pabon',
+			'picture': 'https://i.pinimg.com/474x/30/09/33/300933d32c514d41a2aba0aac3839833.jpg'
+		},
+		'timestamp': datetime.now().strftime("%b %d, %Y - %H:%M horas"),
+		'photo': 'https://hablemosdeculturas.com/wp-content/uploads/2018/12/cerro-de-las-tres-cruces-en-cali-1-e1544207937949.jpeg',
 	}
 ]
 
 def list_posts(request):
 	"""List existing posts."""
-	content = []
-	for post in posts:
-		content.append("""
-			<p><strong>{name}</strong></p>
-			<p><small>{user} - <i>{timestamp}</i></small></p>
-			<figure><img src="{picture}"/></figure>
-		""".format(**post))
-	return HttpResponse('<br>'.join(content))
+	return render(request, 'feed.html', {'posts': posts})
