73e361a480ce11e90a4968864d5800eb0bc48121
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2021-01-23 11:27:14 -0500

    Add posts to admin site - first version

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,47 @@
+"""User admin classes."""
+
+# Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	"""Post admin."""
+
+	list_display = ('user', 'profile', 'title', 'photo')
+
+	search_fields = (
+		'user__email',
+		'user__username',
+		'user__first_name',
+		'user__last_name',
+		'title'
+	)
+
+	list_filter = (
+		'user__email',
+		'user__username',
+	)
+
+	fieldsets = (
+		('Owner', {
+			'fields': (('profile', 'user'),),
+		}),
+		('Post', {
+			'fields': (
+				('title'),
+				('photo')
+			),
+		}),
+		('Metadata', {
+			'fields': (('created', 'modified'),),
+		}),
+	)
+
+	readonly_fields = ('created', 'modified')
