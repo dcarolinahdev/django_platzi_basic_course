b73bac963a5997fdc027d4e43e2f7a5baccee2fe
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2021-01-27 10:03:59 -0500

    Change function update_profile by Class-based view: UpdateView

--- users/views.py
+++ users/views.py
@@ -6,14 +6,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import DetailView, FormView
+from django.views.generic import DetailView, FormView, UpdateView
 
 # Models
 from django.contrib.auth.models import User
+from users.models import Profile
 from posts.models import Post
 
 # Forms
-from users.forms import ProfileForm, SignupForm
+from users.forms import SignupForm
 
 
 class SignupView(FormView):
@@ -48,39 +49,6 @@ def logout_view(request):
 	logout(request)
 	return redirect('users:login')
 
-@login_required
-def update_profile(request):
-	"""Update a user's profile view."""
-	profile = request.user.profile
-
-	if request.method == 'POST':
-		form = ProfileForm(request.POST, request.FILES)
-		if form.is_valid():
-			data = form.cleaned_data
-
-			profile.website = data['website']
-			profile.phone_number = data['phone_number']
-			profile.biography = data['biography']
-			profile.picture = data['picture']
-			profile.save()
-
-			url = reverse('users:detail', kwargs={'username': request.user.username})
-
-			return redirect(url)
-
-	else:
-		form = ProfileForm()
-
-	return render(
-		request=request,
-		template_name='users/update_profile.html',
-		context={
-			'profile': profile,
-			'user': request.user,
-			'form': form
-		}
-	)
-
 class UserDetailView(LoginRequiredMixin, DetailView):
 	"""User detail view."""
 
@@ -96,3 +64,19 @@ class UserDetailView(LoginRequiredMixin, DetailView):
 		user = self.get_object()
 		context['posts'] = Post.objects.filter(user=user).order_by('-created')
 		return context
+
+class UpdateProfileView(LoginRequiredMixin, UpdateView):
+	"""Update profile view."""
+
+	template_name = 'users/update_profile.html'
+	model = Profile
+	fields = ['website', 'biography', 'phone_number', 'picture']
+
+	def get_object(self):
+		"""Return user's profile."""
+		return self.request.user.profile
+
+	def get_success_url(self):
+		"""Return to user's profile."""
+		username = self.object.user.username
+		return reverse('users:detail', kwargs={'username': username})
